faa61d14630ca211887d61142a0b57b4cca6f7c4
Niunan <besokuser@163.com>
2019-06-24 19:03:51 +0700

    goods

--- src/main.js
+++ src/main.js
@@ -4,29 +4,48 @@ import router from "./router";
 import store from "./store";
 
 // axios
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-Vue.use(VueAxios,axios)
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
 // mui
-import './mui/css/mui.min.css';
-import './mui/css/icons-extra.css';
+import "./mui/css/mui.min.css";
+import "./mui/css/icons-extra.css";
 
-
+// ant design;
+import { Icon, Anchor } from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+Vue.component(Icon.name, Icon);
+Vue.component(Anchor.name, Anchor);
 
 // mint ui
-import { Swipe, SwipeItem,Button  } from 'mint-ui';
-import 'mint-ui/lib/style.css' //轮播需要用的样式,直接引入就行
+import {
+  Swipe,
+  SwipeItem,
+  Button,
+  Navbar,
+  TabItem,
+  TabContainer,
+  TabContainerItem,
+  Header,
+  Cell
+} from "mint-ui";
+import "mint-ui/lib/style.css"; //轮播需要用的样式,直接引入就行
 
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
 Vue.component(Button.name, Button);
+Vue.component(Navbar.name, Navbar);
+Vue.component(TabItem.name, TabItem);
+Vue.component(TabContainer.name, TabContainer);
+Vue.component(TabContainerItem.name, TabContainerItem);
+Vue.component(Header.name, Header);
+Vue.component(Cell.name, Cell);
 
 //配置ajax访问路径
 
-axios.defaults.baseURL = 'http://129.28.178.213:8001/';
+axios.defaults.baseURL = "http://129.28.178.213:8001/";
 
 Vue.config.productionTip = false;
-
 
 new Vue({
   router,
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,7 +1,73 @@
 <template>
   <div class="detail">
-    <div class="detali-header">
-      <img src="../img/detail/background1.webp" alt>
+    <nav class="detali-header">
+      <!-- <img src="../img/detail/background1.webp" alt> -->
+      <a class="mui-icon mui-icon-arrowleft"></a>
+    </nav>
+    <!-- 店招 -->
+    <div class="detail-logo">
+      <div class="logo">
+        <span>品牌</span>
+        <img src="../img/detail/logo.webp" alt>
+      </div>
+      <div class="detail-content">
+        <h4>
+          肯德基宅急送 (双桥店)
+          <a-icon type="caret-right"/>
+        </h4>
+        <div class="content-text">
+          <span>评分4.6</span>
+          <span>月销售842单</span>
+          <span>商家配送约30分钟</span>
+        </div>
+      </div>
+      <div class="announcement">
+        <div class="top">
+          <div class="top-left">
+            <span class="tejia">特价</span>
+            <span>特价商品23元起</span>
+          </div>
+          <div class="top-right">
+            <span>3个优惠</span>
+            <a-icon type="caret-down"/>
+          </div>
+        </div>
+        <p class="bottom">公告：本餐厅不使用饿了么配送，由肯德基宅急送官方品牌配送，会员用户无法享受免配送费服务</p>
+      </div>
+    </div>
+
+    <!-- 选项卡 -->
+    <div class="navbar">
+      <mt-navbar :fixed="isFixed" id="fixedHeaderRoot" v-model="selected">
+        <mt-tab-item id="1">点评</mt-tab-item>
+        <mt-tab-item id="2">评价</mt-tab-item>
+        <mt-tab-item id="3">商家</mt-tab-item>
+      </mt-navbar>
+
+      <!-- tab-container -->
+      <mt-tab-container v-model="selected">
+        <mt-tab-container-item id="1">
+          <div class="dianping">
+            <div class="dp-header">
+              <img src="../img/detail/background2.webp" alt>
+            </div>
+            <!-- 锚点 -->
+
+            <a-anchor>
+              <a-anchor-link href="#components-anchor-demo-basic" title="Basic demo"/>
+              <a-anchor-link href="#components-anchor-demo-static-anchor" title="Fixed demo"/>
+              <a-anchor-link href="#API" title="API">
+                <a-anchor-link href="#Anchor-Props" title="Anchor Props"/>
+                <a-anchor-link href="#Link-Props" title="Link Props"/>
+              </a-anchor-link>
+            </a-anchor>
+          </div>
+        </mt-tab-container-item>
+        <mt-tab-container-item id="2">
+          <mt-cell v-for="n in 20" :title="'选项 ' + n"/>
+        </mt-tab-container-item>
+        <mt-tab-container-item id="3"></mt-tab-container-item>
+      </mt-tab-container>
     </div>
   </div>
 </template>
@@ -10,13 +76,146 @@
 export default {
   name: "Detail",
   data() {
-    return {};
+    return {
+      selected: "1",
+      isFixed: false
+    };
   },
-  created() {},
-  methods: {},
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+    this.$nextTick(function() {
+      // 这里fixedHeaderRoot是吸顶元素的ID
+      let header = document.getElementById("fixedHeaderRoot");
+      // 这里要得到top的距离和元素自身的高度
+      this.offsetTop = header.offsetTop + 100; //不知道为什么刚好差100
+      this.offsetHeight = header.offsetHeight;
+      // console.log("offsetTop:" + this.offsetTop + "," + this.offsetHeight);
+    });
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      // 得到页面滚动的距离
+      let scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      // 判断页面滚动的距离是否大于吸顶元素的位置
+      this.headerFixed = scrollTop > this.offsetTop - this.offsetHeight - 50;
+      // console.log(this.headerFixed);
+      if (this.headerFixed) this.isFixed = true;
+      else this.isFixed = false;
+    }
+  },
+  components: {},
   props: []
 };
 </script>
 
 <style lang="scss" scoped>
+.detali-header {
+  background-image: url("../img/detail/background1.webp");
+  background-size: cover;
+  height: 100px;
+  background-position: 0 -12px;
+  position: relative;
+  .mui-icon-arrowleft {
+    position: absolute;
+    left: 0;
+    color: #fff;
+  }
+}
+.detali-header::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: linear-gradient(
+    0deg,
+    hsla(0, 0%, 100%, 0),
+    rgba(0, 0, 0, 0.5)
+  );
+}
+.detail-logo {
+  .logo {
+    position: absolute;
+    top: 37px;
+    left: 150px;
+    span {
+      position: absolute;
+      color: #6f3f15;
+      background-image: linear-gradient(90deg, #fff100, #ffe339);
+      padding: 0 2px;
+      font-size: 0.6rem;
+      border-radius: 2px;
+    }
+    img {
+      width: 80px;
+      height: 80xp;
+      box-shadow: 0 0 0.4vw 0 rgba(0, 0, 0, 0.2);
+      border-radius: 2px;
+    }
+  }
+}
+.detail-content {
+  margin-top: 35px;
+  h4 {
+    font-weight: 600;
+    font-size: 1.3rem;
+  }
+  .content-text span {
+    font-size: 0.65rem;
+    margin-right: 10px;
+    color: #666;
+  }
+}
+.announcement {
+  .top {
+    display: flex;
+    justify-content: space-around;
+    .top-left {
+      .tejia {
+        background-color: rgb(241, 136, 79);
+        color: #fff;
+        font-size: 0.6rem;
+        padding: 1px 5px;
+        margin-right: 5px;
+      }
+      span:last-child {
+        font-size: 0.8rem;
+      }
+    }
+    .top-right {
+      font-size: 0.8rem;
+      color: #999;
+    }
+  }
+  .bottom {
+    font-size: 0.8rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin: 5px 45px;
+  }
+}
+// 选项卡样式bug
+.mint-navbar .mint-tab-item.is-selected {
+  margin-bottom: 0;
+}
+.navbar {
+  text-align: start;
+  .dianping {
+    .dp-header {
+      padding: 5px 10px;
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
+// 点评
 </style>
